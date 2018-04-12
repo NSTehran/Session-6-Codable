8f86f2f30bd64622df70725abbc6094a4a44f52b
Farshad Mousalou <farshadm90@gmail.com>
2018-04-12 14:53:52 +0430

    Initial Commit

---
+++ CodableExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodableExample
+//
+//  Created by Farshad Mousalou on 4/12/18.
+//  Copyright © 2018 Farshad Mousalou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
